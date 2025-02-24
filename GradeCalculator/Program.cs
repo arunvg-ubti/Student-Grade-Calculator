f1e815108ef1d3984745b58dfc5fd77ab35503ac
Arun VG. <arun.vg@ubtiinc.com>
2025-02-24 15:25:19 +0530

    Initial Commit

---
+++ GradeCalculator/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+/*
+ - Develop a console application that calculates the average grade of a student.
+ - Use arrays to store grades and loops to iterate through them.
+ - Implement methods to calculate the average and determine the grade (A, B, C, etc.).
+*/
+
