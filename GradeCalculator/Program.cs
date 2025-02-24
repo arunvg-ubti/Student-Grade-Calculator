4a99f186eee130d4d83177118e288a5054eedb17
Arun VG. <arun.vg@ubtiinc.com>
2025-02-24 17:12:36 +0530

    feat: A new console application for student grade calculation is built

--- GradeCalculator/Program.cs
+++ GradeCalculator/Program.cs
@@ -7,3 +7,60 @@
  - Implement methods to calculate the average and determine the grade (A, B, C, etc.).
 */
 
+//Prompting the user for marks input
+Console.WriteLine("Enter the number of student marks to be entered: ");
+string number=Console.ReadLine()!;
+
+//Datatype conversion from string to int
+int n=int.Parse(number);
+
+//declaration and initialization of array
+int[] marks=new int[n];
+
+//for loop
+for(int i=0;i<n;i++)
+{
+    Console.WriteLine($"Enter Mark {i+1}: ");
+    marks[i]=int.Parse(Console.ReadLine()!);
+}
+
+//looping through array elements to compute the total marks of a student
+int sum=0;
+for(int i=0;i<n;i++)
+{
+    sum+=marks[i];
+}
+
+//Calculating the average marks of a student
+float average=(float)sum/n;
+Console.WriteLine($"The Average Mark is {average:F3}");
+
+//Assigning grades to the student according to the average mark scored by the student
+if(average>=90 && average<=100)
+{
+    Console.WriteLine("The grade is 'O'");
+}
+else if(average>=80 && average<90)
+{
+    Console.WriteLine("The grade is 'A'");
+}
+else if(average>=70 && average<80)
+{
+    Console.WriteLine("The grade is 'B'");
+}
+else if(average>=60 && average<70)
+{
+    Console.WriteLine("The grade is 'C'");
+}
+else if(average>=50 && average<60)
+{
+    Console.WriteLine("The grade is 'D'");
+}
+else if(average>=40 && average<50)
+{
+    Console.WriteLine("The grade is 'E'");
+}
+else
+{
+    Console.WriteLine("Fail");
+}
--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Student-Grade-Calculator
+- The user is prompted for input of marks
+- An array of marks is created
+- The array is looped and iterated over to find the Total Marks (sum) of a student
+- The Average Mark of a student is then computed
+- With a set of if-else block statements, a grade is assigned to the student based on the average mark scored
+- And the desired grades are displayed to the user based on the mark category the average mark falls under
+- usage of arrays, for loops, string interpolation and if-else block statements are used to arrive at the result
+- the end
